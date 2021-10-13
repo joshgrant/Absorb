eab29f2a1e92392ecd178ff4df6c421b69a86b53
Joshua Grant <joshgrant@me.com>
2021-10-13 10:12:35 -0700

    Adding some enemies and a game over condition

--- Absorb/GameScene.swift
+++ Absorb/GameScene.swift
@@ -17,8 +17,8 @@ class GameScene: SKScene
         static let frameDuration: CGFloat = 1.0 / 60.0
     }
     
-//    let ball = Ball(radius: 120, position: CGPoint(x: 200, y: 400))
-//    let otherBall = Ball(radius: 150, position: CGPoint(x: 100, y: 300))
+    private var playerRadius: CGFloat = Constants.referenceRadius
+    
     public let player: Ball = {
         let ball = Ball(radius: Constants.referenceRadius,
                         position: CGPoint(x: 300, y: 0))
@@ -33,9 +33,14 @@ class GameScene: SKScene
         
         physicsWorld.gravity = .zero
         
-//        addChild(ball)
-//        addChild(otherBall)
         addChild(player)
+        
+        run(.repeatForever(.sequence([.wait(forDuration: 0.5),
+                                      .run { [unowned self] in
+            self.addChild(Ball(radius: Constants.referenceRadius - 10,
+                               position: self.player.position + .init(x: .random(in: 100 ... 300),
+                                                                      y: .random(in: 100 ... 400))))
+        }])))
         
         addCamera()
     }
@@ -82,6 +87,13 @@ class GameScene: SKScene
         
         let npcScale = Constants.referenceRadius / currentRadius
         
+        playerRadius += currentRadius - Constants.referenceRadius
+        
+        if playerRadius < 1 {
+            // Game Over
+            assertionFailure()
+        }
+        
         // 2 for loops is expensive... at least this isn't a nested loop
         iterateNPCs { ball in
             ball.applyCameraZoom(scale: npcScale, cameraPosition: player.position)
@@ -95,7 +107,7 @@ class GameScene: SKScene
         super.didFinishUpdate()
         checkGameOver()
     }
-
+    
     // TODO: - Utility
     
     public func iterateNPCs(handler: (Ball) -> Void)
@@ -153,7 +165,7 @@ class GameScene: SKScene
         
         if !camera.position.equalTo(player.position, allowedDelta: 1.0)
         {
-            camera.run(.move(to: player.position, duration: 0.8))
+            camera.run(.move(to: player.position, duration: 0.5))
         }
     }
     
